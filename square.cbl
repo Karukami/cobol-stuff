fe9419924277e28113ff601e0f6e036fe6bc1228
Karukami1 <lobsangbarrigatapia@gmail.com>
2023-04-13 00:28:58 -0400

    Added square.cbl

---
+++ square.cbl
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. "SQUARE".
+       AUTHOR. LOBSANG BARRIGA.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+       MAIN.
+           DISPLAY "+ + + + +".
+           DISPLAY "+       +".
+           DISPLAY "+       +".
+           DISPLAY "+       +".
+           DISPLAY "+ + + + +".
+           STOP RUN.
